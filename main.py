f9b53d03c6b67a09df8915e05d0b015a0f69841b
Igor Ferreira Araujo <igor.faraujo@dinfor.uniceub>
2024-10-24 20:36:24 -0300

    app

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App 
+from kivy.lang import Builder
+
+GUI = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return GUI
+    
+
+MainApp().run()
